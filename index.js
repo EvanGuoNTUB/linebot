80244d5c6dfe5d977614a1ba5ade420a3cc312df
Evan <rrr4408rrr@gmail.com>
2018-10-24 17:45:18 +0800

    20181024

--- index.js
+++ index.js
@@ -20,39 +20,8 @@ var bot = linebot({
 //--------------------------------
 bot.on('message', function(event) {
     event.reply({
-  "type": "template",
-  "altText": "This is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "https://evan01.herokuapp.com/imgs/下載.jpg",
-      "imageAspectRatio": "rectangle",
-      "imageSize": "cover",
-      "imageBackgroundColor": "#FFFFFF",
-      "title": "Menu",
-      "text": "Please select",
-      "defaultAction": {
-          "type": "uri",
-          "label": "View detail",
-          "uri": "http://example.com/page/123"
-      },
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
-          }
-      ]
-  }
+    "type": "text",
+    "text": "Hello, world"
 });  
 });
 
